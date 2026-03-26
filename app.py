700abcef29a9a8dc513f34daf68db61571a79422
Usov_Nikolay <gbaty3a@gmail.com>
2026-03-26 11:13:53 +0300

    ver2

--- app.py
+++ app.py
@@ -7,21 +7,14 @@ st.write("Новая кредитная карта с мгновенным од�
 with st.form("Подать заявку"):
     age = st.number_input("Ваш возраст", min_value=18)
     income = st.number_input("Ваш доход в тысячах рублей", min_value=0)
-    education = st.checkbox("У меня есть высшее образование")
     work = st.checkbox("У меня есть стабильная работа")
     car = st.checkbox("У меня есть автомобиль")
     submit = st.form_submit_button("Подать заявку")
 
 if submit:
-    data = {
-        "age": age,
-        "income": income,
-        "education": education,
-        "work": work,
-        "car": car,
-    }
+    data = {"age": age, "income": income, "good_work_flg": work, "car_own_flg": car}
     response = requests.post("http://127.0.0.1:8000/score", json=data)
     if response.json()["approved"]:
         st.success("Поздравляем, ваша заявка одобрена!")
     else:
-        st.success("Подобрали для дебетовую карту с 3% кэшбеком.")
+        st.success("Подобрали для вас альтернативу — дебетовая карта с 3% кэшбеком.")
--- service.py
+++ service.py
@@ -1,25 +1,22 @@
 import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
-import uvicorn
+
 
 class ClientData(BaseModel):
     income: float
     age: int
     car_own_flg: bool
     good_work_flg: bool
-    
+
 
 app = FastAPI()
 model = joblib.load("model.pkl")
 
+
 @app.post("/score")
 def score(data: ClientData):
-    features = [data.income, data.age, data.car_own_flg, data.good_work_flg]
-    approved = model.predict([features])[0].item()
+    features = [data.income, data.age, data.good_work_flg, data.car_own_flg]
+    approved = not model.predict([features])[0].item()
     return {"approved": approved}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
  
